ad224a1e468ec865385b368d6cd88022f31e2ab4
Taylor Keazirian <taylork26@gmail.com>
2021-04-26 23:18:51 -0500

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Initial Test', () => {
+	it('App renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>,
+			div
+		);
+		ReactDOM.unmountComponentAtNode(div);
+	});
 });
