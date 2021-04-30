21af8b5216496a14585b9f7836d8cb9653dc8ac5
Taylor Keazirian <taylork26@gmail.com>
2021-04-30 11:56:16 -0500

    fixed patch

--- src/components/edit/EditMeditation.js
+++ src/components/edit/EditMeditation.js
@@ -92,7 +92,6 @@ class EditMeditation extends React.Component {
 				if (!res.ok) {
 					return res.json().then(error => Promise.reject(error));
 				}
-				return res.json();
 			})
 			.then(() => {
 				this.context.editMeditation(editedMeditation);
