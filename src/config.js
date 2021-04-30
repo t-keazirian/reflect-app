2ea06b7ff831e3bb90172a8c700241530abb834b
Taylor Keazirian <taylork26@gmail.com>
2021-04-29 19:52:17 -0500

    config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
 	API_BASE_URL:
 		process.env.NODE_ENV === 'development'
 			? 'http://localhost:8000/api/reflections'
-			: 'https://damp-garden-53690.herokuapp.com/api',
+			: 'https://damp-garden-53690.herokuapp.com/api/reflections',
 };
 
 export default config;
